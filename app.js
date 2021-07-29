51a83bed8da82f4edc5c23df5311c747d08cf8a2
LokiDieKatze <sarah.marques.fr@gmail.com>
2021-07-29 20:03:28 +0200

    rest-api

--- app.js
+++ app.js
@@ -1,26 +1,26 @@
-const express = require('express')
-const db = require('./mydb')
+const express = require("express");
+const db = require("./mydb");
 
-const IP = '192.168.0.10'
-const PORT = 3333
+const IP = "172.24.175.1";
+const PORT = 3333;
 
-const app = express()
+const app = express();
 
 // A middle for checking if an api key is provided by the user
 // If an api key is provided in the authorization header field then
 // the api key is attached to the req object
 const getApiKey = async (req, res, next) => {
-  const apiKey = req.headers.authorization
+  const apiKey = req.headers.authorization;
   if (!apiKey) {
     res.status(403).json({
-      status: 'fail',
-      data: { apiKey: 'No api key in Authorization header' },
-    })
+      status: "fail",
+      data: { apiKey: "No api key in Authorization header" },
+    });
   } else {
-    req.apiKey = apiKey.replace('Bearer ', '').trim()
-    next()
+    req.apiKey = apiKey.replace("Bearer ", "").trim();
+    next();
   }
-}
+};
 
 // A middleware for checking if an api key is valid
 // and is still active.
@@ -28,93 +28,132 @@ const getApiKey = async (req, res, next) => {
 
 const validateApiKey = async (req, res, next) => {
   try {
-    const result = await db.getUserByApiKey(req.apiKey)
+    const result = await db.getUserByApiKey(req.apiKey);
     // Check if user is active
     // check if null result then not found
     if (!result || !result.active) {
-      res.status(403).json({ status: 'fail', data: { key: 'Invalid api key' } })
+      res
+        .status(403)
+        .json({ status: "fail", data: { key: "Invalid api key" } });
     } else {
-      req.userId = result.id
-      next()
+      req.userId = result.id;
+      next();
     }
   } catch (e) {
-    console.log(e)
-    res.status(500).json({ code: 'error', message: 'Internal server error' })
+    console.log(e);
+    res.status(500).json({ code: "error", message: "Internal server error" });
   }
-}
+};
 
-app.use(express.urlencoded({ extended: false })) // to support URL-encoded bodies
-app.use(express.json()) // to support JSON-encoded bodies
+app.use(express.urlencoded({ extended: false })); // to support URL-encoded bodies
+app.use(express.json()); // to support JSON-encoded bodies
 
-app.post('/register', async (req, res) => {
-  const username = req.body.username
-  const email = req.body.email
+app.post("/register", async (req, res) => {
+  const username = req.body.username;
+  const email = req.body.email;
   try {
-    const result = await db.register(username, email)
+    const result = await db.register(username, email);
     res.json({
-      status: 'success',
+      status: "success",
       data: { id: result.id, key: result.apiKey.key },
-    })
+    });
   } catch (e) {
-    if (e.status === 'fail') {
-      res.status(400).json({ status: e.status, data: e.dataError })
+    if (e.status === "fail") {
+      res.status(400).json({ status: e.status, data: e.dataError });
     } else {
       // e.status === 50X
-      res.status(500).json({ status: e.status, message: e.message })
+      res.status(500).json({ status: e.status, message: e.message });
     }
   }
-})
+});
 
-app.use(getApiKey)
-app.use(validateApiKey)
+app.use(getApiKey);
+app.use(validateApiKey);
 
-app.get('/user_by_id/:userId', async (req, res) => {
-  let userId = req.params.userId
+app.get("/user_by_id/:userId", async (req, res) => {
+  let userId = req.params.userId;
   if (isNaN(userId)) {
-    res.json({ status: 'fail', data: { userId: `${userId} is not a number` } })
-    return
+    res.json({ status: "fail", data: { userId: `${userId} is not a number` } });
+    return;
   }
-  userId = Number(userId)
+  userId = Number(userId);
   try {
-    const result = await db.getUserById(userId)
-    res.json({ status: 'success', data: { user: result } })
+    const result = await db.getUserById(userId);
+    res.json({ status: "success", data: { user: result } });
   } catch (e) {
-    if (e.status === 'fail') {
-      res.status(400).json({ status: e.status, data: e.dataError })
+    if (e.status === "fail") {
+      res.status(400).json({ status: e.status, data: e.dataError });
     } else {
       // e.status === 50X
-      res.status(500).json({ status: e.status, message: e.message })
+      res.status(500).json({ status: e.status, message: e.message });
     }
   }
-})
+});
 
-app.get('/myinfo', async (req, res) => {
-  const userId = req.userId
+app.get("/myinfo", async (req, res) => {
+  const userId = req.userId;
   try {
-    const result = await db.getUserById(userId)
-    res.json({ status: 'success', data: { user: result } })
+    const result = await db.getUserById(userId);
+    res.json({ status: "success", data: { user: result } });
   } catch (e) {
-    if (e.status === 'fail') {
-      res.status(400).json({ status: e.status, data: e.dataError })
+    if (e.status === "fail") {
+      res.status(400).json({ status: e.status, data: e.dataError });
     } else {
       // e.status === 50X
-      res.status(500).json({ status: e.status, message: e.message })
+      res.status(500).json({ status: e.status, message: e.message });
     }
   }
-})
+});
 
-app.get('/user_by_username/:username', async (req, res) => {
+app.get("/user_by_username/:username", async (req, res) => {
   // A implementer
-})
+  try {
+    const result = await db.getUserByUsername(req.params.username);
+    res.json({ status: "success", data: { user: result } });
+  } catch (e) {
+    if (e.status === "fail") {
+      res.status(400).json({ status: e.status, data: e.dataError });
+    } else {
+      res.status(500).json({ status: e.status, message: e.message });
+    }
+  }
+});
 
-app.post('/send_message/:username', async (req, res) => {
+app.post("/send_message/:username", async (req, res) => {
   // A implementer
-})
+  const srcId = Number(req.userId);
+  const dst = await db.getUserByUsername(req.params.username);
+  const dstId = dst.id;
+  const content = req.body.content;
+  try {
+    const result = await db.sendMessage(srcId, dstId, content);
+    res.json({
+      status: "success",
+      data: { from: result.srcId, to: result.dstId, content: result.content },
+    });
+  } catch (e) {
+    if (e.status === "fail") {
+      res.status(400).json({ status: e.status, data: e.dataError });
+    } else {
+      res.status(500).json({ status: e.status, message: e.message });
+    }
+  }
+});
 
-app.get('/read_message/', async (req, res) => {
+app.get("/read_message/", async (req, res) => {
   // A implementer
-})
+  try {
+    const result = await db.readMessage(req.userId);
+    res.json({ status: "success", data: result });
+  } catch (e) {
+    if (e.status === "fail") {
+      res.status(400).json({ status: e.status, data: e.dataError });
+    } else {
+      res.status(500).json({ status: e.status, message: e.message });
+    }
+  }
+});
 
 app.listen(PORT, IP, () => {
-  console.log(`listening on ${IP}:${PORT}`)
-})
+  console.log(`listening on ${IP}:${PORT}`);
+});
